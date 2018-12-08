676d0a1f7fb5422afa4feffe465ff66ba27734b5
jethet <henriette.hettinga@gmail.com>
2018-12-08 15:38:13 +0100

    Create first CB exercise

---
+++ 1_SleepIn.py
@@ -0,0 +1,6 @@
+"""
+Parameter weekday is True if it is a weekday
+Parameter vacation is True if it is a vacation weekday
+We sleep in if it is not a weekday or if we're on vacation
+Return True if we sleep in
+"""
