d15ea4eabed8e0d9cdf1bd6b50ac8578d5b247c6
jethet <henriette.hettinga@gmail.com>
2018-12-08 15:52:39 +0100

    CodingBat warmup-1

--- 1_SleepIn.py
+++ 1_SleepIn.py
@@ -4,3 +4,9 @@ Parameter vacation is True if it is a vacation weekday
 We sleep in if it is not a weekday or if we're on vacation
 Return True if we sleep in
 """
+
+def sleep_in(weekday, vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False
