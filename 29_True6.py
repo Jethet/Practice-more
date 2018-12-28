c669616ec12c13d8d34c49da05c4a0aafff3cccb
jethet <henriette.hettinga@gmail.com>
2018-12-28 16:27:40 +0100

    CodingBat list-29

---
+++ 29_True6.py
@@ -0,0 +1,10 @@
+# Given an array of ints, return True if 6 appears as either the first
+# or the last element in the array.
+
+def first_last6(nums):
+    if nums[0] or nums[-1]:
+        return True
+
+print(first_last6([1, 2, 6]))
+print(first_last6([6, 1, 2, 3]))
+print(first_last6([13, 6, 2, 3]))
