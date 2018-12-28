aa3c92a98d9b998cbe386364654010cd0074ca3d
jethet <henriette.hettinga@gmail.com>
2018-12-28 18:08:15 +0100

    CodingBat list-35

---
+++ 35_Reverse.py
@@ -0,0 +1,8 @@
+# Given an array of three ints, return a new array with the elements in
+# reversed order.
+
+def reverse3(nums):
+    return nums[::-1]
+
+print(reverse3([1,2,3]))
+print(reverse3([5, 11, 9]))
