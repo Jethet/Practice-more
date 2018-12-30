9747c4b7e5a4f1e89f956f9b9b9c9a4ac68c12a2
jethet <henriette.hettinga@gmail.com>
2018-12-30 16:36:11 +0100

    CodingBat Logic1-47

--- 47_Six.py
+++ 47_Six.py
@@ -2,13 +2,15 @@
 # their sum or difference is 6 (the function abs(num) computes absolute value)
 
 def six(a, b):
-    if a or b == 6:
+    if abs(a) == 6:
         return True
-    elif a + b == 6:
+    elif abs(b) == 6:
         return True
-    elif a - b == 6:
+    elif abs(a + b) == 6:
         return True
-    elif b - a == 6:
+    elif abs(a - b) == 6:
+        return True
+    elif abs(b - a) == 6:
         return True
     else:
         return False
