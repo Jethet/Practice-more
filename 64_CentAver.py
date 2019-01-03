ace647155ed1eff95db7b1227c369da59ea0970f
jethet <henriette.hettinga@gmail.com>
2019-01-03 22:07:23 +0100

    CodingBat List2-64

---
+++ 64_CentAver.py
@@ -0,0 +1,16 @@
+# Return the 'centered' average of an array of ints, which we'll say is the
+# mean average of the values, except ignoring the largest and smallest values
+# in the array. If there are multiple copies of the smallest value, ignore
+# just one copy, and likewise for the largest value. Use int division to
+# produce the final average. You may assume that the array is length 3 or more.
+
+def centered_average(nums):
+    count = len(nums) - 2
+    x = max(nums)
+    y = min(nums)
+    return (sum(nums) - (x + y)) / count
+
+
+print(centered_average([1, 2, 3, 4, 100]))      # 3
+print(centered_average([1, 1, 5, 5, 10, 8, 7]))  # 5
+print(centered_average([-10, -4, -2, -4, -2, 0])) # -3
