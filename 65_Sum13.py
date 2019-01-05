4bce438b7fa7c9345c855d97c7193c90a3b40ea9
jethet <henriette.hettinga@gmail.com>
2019-01-05 20:44:12 +0100

    CodingBat List2-65

--- 65_Sum13.py
+++ 65_Sum13.py
@@ -2,8 +2,6 @@
 # Except the number 13 is very unlucky, so it does not count and numbers that
 # come immediately after a 13 also do not count.
 
-
-
 def sum13(nums):
     sum = 0
     if len(nums) > 0 and nums[0] != 13:
@@ -12,7 +10,10 @@ def sum13(nums):
             if nums[i] != 13 and nums[i -1] != 13:
                 sum += nums[i]
         return sum
-        """
+
+"""
+MY OWN ATTEMPT:
+
 def sum13(nums):
     sum = 0
     if nums[0] != 13:
