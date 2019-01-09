d3f471a86b35a2879b3f1d9c10d1e4f22fd7563b
jethet <henriette.hettinga@gmail.com>
2019-01-09 20:50:04 +0100

    Edabit first level

--- EdabitStart.py
+++ EdabitStart.py
@@ -1,5 +1,4 @@
 # Find the total number of digits a given number has
-
 def find_digit_amount(num):
 	return len(str(num))
 
@@ -7,8 +6,14 @@ print(find_digit_amount(15))   # = 2
 
 # Create a function that takes a list and a string as arguments
 # and return the index of the string.
-
 def find_index(lst, txt):
 	return lst.index(txt)
 
 print(find_index(['abc', 'cde', 'efg'], 'cde'))   # = 1
+
+# Create a function that takes a string and returns the word count.
+# The string will be a sentence.
+def count_words(txt):
+	return len(txt.split())
+
+print(count_words('Did you see that coming?'))   # = 5
