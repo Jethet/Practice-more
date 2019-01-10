dfd40e39d5f74ae9bdf619a59f62d5fb1cecb654
jethet <henriette.hettinga@gmail.com>
2019-01-10 12:55:18 +0100

    PyNative exer. 1

--- PN1ProductSum.py
+++ PN1ProductSum.py
@@ -1,10 +1,14 @@
 # Accept two int values from user and return their product.
 # If the product is greater than 1000, then return their sum
 
+def mult_num(value1, value2):
+    sum_mult = value1 * value2
+    if sum_mult <= 1000:
+        return sum_mult
+    if sum_mult > 1000:
+        return value1 + value2
 
 value1 = int(input("Please enter the first integer. "))
 value2 = int(input("Please enter the second integer. "))
-if value1 * value2 <= 1000:
-    print(value1*value2)
-if value1 * value2 > 1000:
-    print(value1 + value2)
+
+print(mult_num(value1, value2))
