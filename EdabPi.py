7902d15c0ff09088df0a7f18e2dc61759e7e4320
jethet <henriette.hettinga@gmail.com>
2019-01-15 13:54:50 +0100

    Edabit exercise Pi

---
+++ EdabPi.py
@@ -0,0 +1,13 @@
+# Given a number n, write a function that returns PI to n decimal places.
+
+import math
+
+def my_pi(n):
+    return round(math.pi, n)
+
+
+print(my_pi(5)) # 3.14159
+
+print(my_pi(4)) # 3.1416
+
+print(my_pi(15)) # 3.141592653589793
