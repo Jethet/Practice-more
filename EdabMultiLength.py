96b1d30e5aed84e40323601e9ae38bf358e1a5c2
jethet <henriette.hettinga@gmail.com>
2019-01-15 17:47:57 +0100

    Edabit multiply values by list length

--- EdabMultiLength.py
+++ EdabMultiLength.py
@@ -2,12 +2,12 @@
 # contained in that list.
 
 def MultiplyByLength(arr):
-    count = 0
-    for x in range(len(arr), -1):
-        count += x
+    count = len(arr)
+    my_list = []
     for x in arr:
-        total = x * count
-    return total
+        x = x * count
+        my_list.append(x)
+    return my_list
 
 
 print(MultiplyByLength([2, 3, 1, 0])) # [8, 12, 4, 0]
