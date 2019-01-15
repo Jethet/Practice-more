9fea12d34767b66c7c60fbc57b4f52dc00d66e1b
jethet <henriette.hettinga@gmail.com>
2019-01-15 14:16:31 +0100

    Edabit exercise repeat

---
+++ EdabRepeat.py
@@ -0,0 +1,16 @@
+# Create a function that takes two arguments (item, times).
+# The first argument (item) is the item that needs repeating while
+# the second argument (times) is the number of times the item is to be repeated.
+# Return the result in a list.
+
+def repeat(item, times):
+    return [item] * times
+
+
+print(repeat("edabit", 3)) # ["edabit", "edabit", "edabit"]
+
+print(repeat(13, 5))   # [13, 13, 13, 13, 13]
+
+print(repeat("7", 2))   # ["7", "7"]
+
+print(repeat(0, 0))       # []
--- Scribbles.py
+++ Scribbles.py
@@ -1,7 +1,4 @@
-def month_name(num):
-    months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', \
-    6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', \
-    11: 'November', 12: 'December'}
-    return months.get(num)
-
-print(month_name(11))
+item = "abc "
+times = 3
+my_list = []
+print(my_list.append(item * times))
