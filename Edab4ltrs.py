4838bf0750d249bd051827cc09f26a99e6df1619
jethet <henriette.hettinga@gmail.com>
2019-01-15 14:39:36 +0100

    Edabit exercises 4 ltr words

---
+++ Edab4ltrs.py
@@ -0,0 +1,17 @@
+# Create a function that takes a list of strings.
+# Return all words in the list that are exactly four letters.
+
+def isFourLetters(lst):
+    my_list = []
+    for x in lst:
+        if len(x) == 4:
+            my_list.append(x)
+    return my_list
+
+
+
+print(isFourLetters(["Ryan", "Kieran", "Jason", "Matt" ])) # ["Ryan", "Matt"]
+
+print(isFourLetters(["Tomato", "Potato", "Pair"]))      # ["Pair"]
+
+print(isFourLetters(["Kangaroo", "Bear", "Fox"]))       # ["Bear"]
