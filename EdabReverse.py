06b9f25b20a328042c4407425a57dd5fe6bde442
jethet <henriette.hettinga@gmail.com>
2019-01-15 14:40:31 +0100

    Edabit reverse text

---
+++ EdabReverse.py
@@ -0,0 +1,13 @@
+# Create a function that takes a string as its argument and returns the string
+# in reversed order.
+
+def reverse(txt):
+    return txt[::-1]
+
+
+
+print(reverse("Hello World")) # "dlroW olleH"
+
+print(reverse("The quick brown fox.")) # ".xof nworb kciuq ehT"
+
+print(reverse("Edabit is really helpful!")) # "!lufpleh yllaer si tibadE"
