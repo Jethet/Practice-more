b8acf42f65d71e9b5e4875049af3b77c3c1f5a4f
jethet <henriette.hettinga@gmail.com>
2019-01-15 13:31:45 +0100

    Edabit exercise LargestValue

---
+++ EdabLargest.py
@@ -0,0 +1,14 @@
+# Create a function that takes a list of numbers. Return the largest number
+# in the list.
+
+def findLargestNum(nums):
+    return max(nums)
+
+
+
+
+print(findLargestNum([4, 5, 1, 3])) # 5
+
+print(findLargestNum([300, 200, 600, 150])) # 600
+
+print(findLargestNum([1000, 1001, 857, 1])) # 1001
