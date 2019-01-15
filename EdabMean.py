9cab86cf423720abdebf8dfbb67877c12ad4ec3a
jethet <henriette.hettinga@gmail.com>
2019-01-15 13:42:05 +0100

    Edabit exercise mean 2 digits

---
+++ EdabMean.py
@@ -0,0 +1,14 @@
+# Create a function that takes a list of numbers and returns the mean value.
+# Round to two decimal places.
+
+def mean(lst):
+    x = len(lst)
+    y = sum(lst)
+    return round((y / x), 2)
+
+
+print(mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3])) # 2.54
+
+print(mean([2, 3, 2, 3])) # 2.50
+
+print(mean([3, 3, 3, 3, 3])) # 3.00
