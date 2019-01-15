fc7438c6ee68622e821abf8f085996e691d7efac
jethet <henriette.hettinga@gmail.com>
2019-01-15 17:59:53 +0100

    Edabit absolute sum of list ints

---
+++ EdabAbsSum.py
@@ -0,0 +1,12 @@
+# Take a list of integers (positive or negative or both) and return the sum of
+# the absolute value of each element.
+
+
+def get_abs_sum(lst):
+    
+
+
+print(get_abs_sum([2, -1, 4, 8, 10])) # 25
+print(get_abs_sum([-3, -4, -10, -2, -3])) # 22
+print(get_abs_sum([2, 4, 6, 8, 10])) # 30
+print(get_abs_sum([-1])) # 1
