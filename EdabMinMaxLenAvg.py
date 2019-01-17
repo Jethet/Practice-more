56a9d50d987af383ffc7dc650406e4015f8e5da3
jethet <henriette.hettinga@gmail.com>
2019-01-17 20:51:44 +0100

    Edabit min, max, length and average of list

---
+++ EdabMinMaxLenAvg.py
@@ -0,0 +1,14 @@
+# Create a function that takes a list of numbers and returns the following:
+#    Minimum Value
+#    Maximum Value
+#    Sequence Length
+#    Average Value
+
+def minMaxLengthAverage(lst):
+    return min(lst), max(lst), len(lst), (sum(lst)/len(lst))
+
+
+
+print(minMaxLengthAverage([6, 9, 15, -2, 92, 11]))  # [-2, 92, 6, 21.833333333333332]
+print(minMaxLengthAverage([30, 43, 20, 92, 3, 74])) # [3, 92, 6, 43.666666666666664]
+print(minMaxLengthAverage([4.54, 8.32, 5.20]))     # [4.54, 8.32, 3, 6.02]
