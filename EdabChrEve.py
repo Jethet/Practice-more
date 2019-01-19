ef99c3c8274bbceffa314c133a6a5977c4a24f4f
jethet <henriette.hettinga@gmail.com>
2019-01-19 17:51:55 +0100

    Edabit datetime challenge

--- EdabChrEve.py
+++ EdabChrEve.py
@@ -1,13 +1,13 @@
 # Create a function that accepts a Date object and returns True if
 # it's Christmas Eve (December 24th) and False otherwise.
 
-def datetime(date):
+from datetime import date
+
+def time_for_milk_and_cookies(date):
     return datetime.date(2019, 12, 24)
 
 
 
-print(datetime.date( 2013, 12, 24 )) # True
-
-print(datetime.date( 2013, 1, 23 ))  # False
-
-print(datetime.date( 3000, 12, 24 )) # True
+print(time_for_milk_and_cookies( 2013, 12, 24 )) # True
+print(time_for_milk_and_cookies( 2013, 1, 23 ))  # False
+print(time_for_milk_and_cookies( 3000, 12, 24 )) # True
