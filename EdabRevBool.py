0f2f3345f92e6a26579fe8001ad87774fee80485
jethet <henriette.hettinga@gmail.com>
2019-01-20 18:47:55 +0100

    Edabit reverse boolean

---
+++ EdabRevBool.py
@@ -0,0 +1,23 @@
+# Create a function that reverses a boolean value and returns the string
+# "boolean expected" if another variable type is given.
+
+def reverse(arg):
+    if arg = bool:
+        return not arg
+    else:
+        return "boolean expected"
+
+# Examples Edabit:
+def reverse(arg):
+	if isinstance(arg, bool):
+		return not arg
+	else:
+		return('boolean expected')
+
+def reverse(arg=None):
+	return not arg if type(arg) == bool else "boolean expected"
+
+print(reverse(True)) # False
+print(reverse(False)) # True
+print(reverse(0)) # "boolean expected"
+print(reverse(None)) # "boolean expected"
