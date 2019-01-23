ab207b69bf943fe8493ba98a17f87bc6c01f0e52
jethet <henriette.hettinga@gmail.com>
2019-01-23 22:54:04 +0100

    Edabit vector magnitude

---
+++ EdabVectorMag.py
@@ -0,0 +1,16 @@
+# You will be implementing a basic case of the map-reduce pattern in programming.
+# Given a vector stored as a list of integers, find the magnitude of the vector.
+# Use the standard distance formula for n-dimensional Cartesian coordinates.
+# The list can have any length.
+# The input list will contain integers (except for empty list [] ➞ 0).
+
+def magnitude(lst):
+
+
+print(magnitude([3, 4])) # 5
+
+print(magnitude([0, 0, -10])) # 10
+
+print(magnitude([])) # 0
+
+print(magnitude([2, 3, 6, 1, 8] )) # 10.677078252031311
