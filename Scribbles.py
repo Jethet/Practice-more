cca8476c27bb7c570bdc68d3f34619d4482bc71b
jethet <henriette.hettinga@gmail.com>
2019-01-23 22:37:14 +0100

    Edabit symmetric value

--- Scribbles.py
+++ Scribbles.py
@@ -1,8 +1,3 @@
-num = [1, 3, 5, 6, 4, 9, 7]
-x = 'whatever'
-y = 'wethear'
-for z in x:
-    if z in y:
-        print("true")
-    else:
-        print("false")
+x = str(2773)
+print(x)
+print(x[::-1])
