75d350beb22b8a7c6a0717f5da43797cae61cb92
jethet <henriette.hettinga@gmail.com>
2019-01-23 21:02:10 +0100

    Edabit return number equal values

---
+++ EdabEqualVal.py
@@ -0,0 +1,12 @@
+# Write a function named equal that takes three input values (a, b, c) and
+# returns the number of equal values. Your function must return 0, 2 or 3.
+
+def equal(a, b, c):
+
+
+
+print(equal(3, 4, 3)) # 2
+
+print(equal(1, 1, 1)) # 3
+
+print(equal(3, 4, 1)) # 0
