739b52708db44b85e1974aa84a8b5cb63114ca59
jethet <henriette.hettinga@gmail.com>
2019-01-23 19:26:12 +0100

    Edabit valid PIN

---
+++ EdabATM.py
@@ -0,0 +1,17 @@
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain
+# anything but exactly 4 digits or exactly 6 digits. Your task is to create
+# a function that takes a string and returns True if the PIN is valid and
+# False if it's not.
+# Some test cases contain special characters. Empty strings must return False.
+
+def is_valid_PIN(pin):
+
+
+
+print(is_valid_PIN("1234")) # True
+
+print(is_valid_PIN("12345")) # False
+
+print(is_valid_PIN("a234")) # False
+
+print(is_valid_PIN("")) # False
