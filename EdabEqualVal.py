758d24d7eab7e634bf6fbb1851779102004f61ea
jethet <henriette.hettinga@gmail.com>
2019-01-23 22:22:32 +0100

    Edabit number of equal values

--- EdabEqualVal.py
+++ EdabEqualVal.py
@@ -2,7 +2,14 @@
 # returns the number of equal values. Your function must return 0, 2 or 3.
 
 def equal(a, b, c):
-
+    count = 0
+    if a == b and b == c and a == c:
+        count = 3
+    elif a == b or b == c or a == c:
+        count = 2
+    else:
+        count = 0
+    return count
 
 
 print(equal(3, 4, 3)) # 2
