27c1b885acdaff1b839e44f0c6d77988e8b75018
jethet <henriette.hettinga@gmail.com>
2019-01-23 19:17:05 +0100

    Edabit string alpha

---
+++ EdabAlpha.py
@@ -0,0 +1,12 @@
+# Create a function that takes a string and returns a string with its letters
+# in alphabetical order.
+
+def alphabet_soup(txt):
+
+
+
+print(alphabet_soup("hello"))  # "ehllo"
+print(alphabet_soup("edabit")) # "abdeit"
+print(alphabet_soup("hacker")) # "acehkr"
+print(alphabet_soup("geek"))   # "eegk"
+print(alphabet_soup("javascript")) # aacijprstv
