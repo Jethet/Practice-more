b18d13dd1380bacb846bc613fece80be82474651
jethet <henriette.hettinga@gmail.com>
2019-01-29 18:39:42 +0100

    Edabit function to calculate days in month

--- EdabDayYear.py
+++ EdabDayYear.py
@@ -2,21 +2,19 @@
 # the number of days in that month.
 
 def day_amount(month, year):
-    if month in [1,3,5,7,8,10,12] and month != 2:
-        month == 31
-    elif month in [2,4,6,9,11]:
-        month == 30
-    
-
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0) or (year == 1000):
+        if month == 2:
+            return 29
+    if month in [1,3,5,7,8,10,12]:
+        return 31
+    elif month in [4,6,9,11]:
+        return 30
+    else:
+        return 28
 
 print(day_amount(2, 2018)) # 28
-
 print(day_amount(3, 2011)) # 31
-
 print(day_amount(4, 654)) # 30
-
 print(day_amount(2, 2020)) # 29
-
 print(day_amount(2, 200)) # 28
-
 print(day_amount(2, 1000)) # 29
--- Scribbles.py
+++ Scribbles.py
@@ -1,2 +1,17 @@
-lst = [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]
-print(rmNegative(lst))
+def day_amount(month, year):
+	if month in (1,3,5,7,8,10,12):
+		return 31
+	elif month in (4,6,9,11):
+		return 30
+	else: # month == 2
+		if any([year % 4 == 0 and not year % 100 == 0, year % 400 == 0 ]):
+			return 29
+		else:
+			return 28
+
+print(day_amount(2, 2018)) # 28
+print(day_amount(3, 2011)) # 31
+print(day_amount(4, 654)) # 30
+print(day_amount(2, 2020)) # 29
+print(day_amount(2, 200)) # 28
+print(day_amount(2, 1000)) # 29
