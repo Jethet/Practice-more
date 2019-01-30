a22fbe5addeabfcf0defa9869fc7e2b055df82ea
jethet <henriette.hettinga@gmail.com>
2019-01-30 18:53:26 +0100

    Edabit check if chars of string are in other string

--- EdabLtrCheck.py
+++ EdabLtrCheck.py
@@ -4,9 +4,15 @@
 # Both strings are presented as a single argument in the form of a list.
 
 def letter_check(lst):
-
-
-
+    lst1 = lst[0]
+    lst2 = lst[1]
+    lst1 = lst1.lower()
+    lst2 = lst2.lower()
+    for char in lst2:
+        if char not in lst1:
+            return False
+    else:
+        return True
 
 print(letter_check(["trances", "nectar"])) # True
 print(letter_check(["compadres", "DRAPES"])) # True
--- Scribbles.py
+++ Scribbles.py
@@ -3,14 +3,14 @@ def letter_check(lst):
     lst2 = lst[1]
     lst1 = lst1.lower()
     lst2 = lst2.lower()
-    if lst2 in lst1:
-            return True
-
+    for char in lst2:
+        if char not in lst1:
+            return False
+    else:
+        return True
 
 
 
 print(letter_check(["trances", "nectar"])) # True
-
 print(letter_check(["compadres", "DRAPES"])) # True
-
 print(letter_check(["parses", "parsecs"])) # False
