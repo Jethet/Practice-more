ec944a65257df37f028370cdd47b9323c15347fd
jethet <henriette.hettinga@gmail.com>
2019-01-30 17:02:19 +0100

    Edabit check if string is in other string

---
+++ EdabLtrCheck.py
@@ -0,0 +1,13 @@
+# Create a function that accepts a list of two strings and checks if the letters
+# in the second string are present in the first string. Function should not be
+# case sensitive (as indicated in the second example).
+# Both strings are presented as a single argument in the form of a list.
+
+def letter_check(lst):
+
+
+
+
+print(letter_check(["trances", "nectar"])) # True
+print(letter_check(["compadres", "DRAPES"])) # True
+print(letter_check(["parses", "parsecs"])) # False
--- Scribbles.py
+++ Scribbles.py
@@ -1,15 +1,16 @@
-def sum_neg(lst):
-	if lst:
-		countPos = len([i for i in lst if i > 0])
-		sumNeg = sum([i for i in lst if i < 0])
-		return [countPos, sumNeg]
-	else:
-		return lst
+def letter_check(lst):
+    lst1 = lst[0]
+    lst2 = lst[1]
+    lst1 = lst1.lower()
+    lst2 = lst2.lower()
+    if lst2 in lst1:
+            return True
 
-print(sum_neg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])) # [10, -65]
 
-print(sum_neg([92, 6, 73, -77, 81, -90, 99, 8, -85, 34])) # [7, -252]
 
-print(sum_neg([91, -4, 80, -73, -28])) # [2, -105]
 
-print(sum_neg([])) # []
+print(letter_check(["trances", "nectar"])) # True
+
+print(letter_check(["compadres", "DRAPES"])) # True
+
+print(letter_check(["parses", "parsecs"])) # False
