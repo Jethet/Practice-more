0654b789c55b3807f70941da98ad8df376f5074a
jethet <henriette.hettinga@gmail.com>
2019-02-03 19:22:13 +0100

    Edabit mask all but last 4 chars of string

--- EdabMaskify.py
+++ EdabMaskify.py
@@ -9,7 +9,7 @@
 
 def maskify(txt):
     if len(txt) == 0:
-        return ''
+        return txt
     if len(txt) < 4:
         return txt
     if len(txt) >= 4:
@@ -18,11 +18,7 @@ def maskify(txt):
         return ('#' * len(new_txt1)) + new_txt2
 
 
-
 print(maskify("4556364607935616")) # "############5616"
-
 print(maskify("64607935616")) # "#######5616"
-
 print(maskify("1")) # "1"
-
 print(maskify("")) # ""
