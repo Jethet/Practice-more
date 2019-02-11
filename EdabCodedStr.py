284ef499a5208455fb892e84a1cefdc3a171872c
jethet <henriette.hettinga@gmail.com>
2019-02-11 18:16:34 +0100

    Edabit hack string chars

--- EdabCodedStr.py
+++ EdabCodedStr.py
@@ -4,11 +4,11 @@
 # and 's's with 5.
 
 def hacker_speak(txt):
-    if 'a' in txt:
-        return txt.replace('a', '4')
+    hack = txt.replace('a','4').replace('e','3').replace('i','1').replace('o','0')\
+            .replace('s','5')
+    return hack
+
 
 print(hacker_speak("javascript is cool")) #➞ "j4v45cr1pt 15 c00l"
-
 print(hacker_speak("programming is fun")) #➞ "pr0gr4mm1ng 15 fun"
-
 print(hacker_speak("become a coder")) #➞ "b3c0m3 4 c0d3r"
