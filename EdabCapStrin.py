c4a794a52e6d00ed3da52f1a74ff92f04d57596a
jethet <henriette.hettinga@gmail.com>
2019-03-07 19:39:50 +0100

    Added test to function

--- EdabCapStrin.py
+++ EdabCapStrin.py
@@ -2,9 +2,9 @@
 # character of each word to uppercase. Return the newly formatted string.
 
 def make_title(txt):
-	return ' '.join(word.capitalize() for word in txt.split())
+	return ' '.join(word[0].upper() + word[1:] for word in txt.split())
 
-	
+print(make_title("i aM a tITLE"))
 print(make_title("This is a title")) # "This Is A Title"
 print(make_title("capitalize every word")) # "Capitalize Every Word"
 print(make_title("I Like Pizza")) # "I Like Pizza"
