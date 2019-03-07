822bcc1aa749808a8baa0555ae296a27ca6524d6
jethet <henriette.hettinga@gmail.com>
2019-03-07 19:31:06 +0100

    Edabit capitalize each word in a string

---
+++ EdabCapStrin.py
@@ -0,0 +1,11 @@
+# Create a function that takes a string as an argument and converts the first
+# character of each word to uppercase. Return the newly formatted string.
+
+def make_title(txt):
+	return ' '.join(word.capitalize() for word in txt.split())
+
+	
+print(make_title("This is a title")) # "This Is A Title"
+print(make_title("capitalize every word")) # "Capitalize Every Word"
+print(make_title("I Like Pizza")) # "I Like Pizza"
+print(make_title("PIZZA PIZZA PIZZA")) # "PIZZA PIZZA PIZZA"
