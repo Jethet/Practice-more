07960245404cba0c978a54dd9164a0fb398ac51e
jethet <henriette.hettinga@gmail.com>
2019-03-10 16:25:27 +0100

    Edabit function to alternate caps and lower case in string

---
+++ EdabAltCaps.py
@@ -0,0 +1,10 @@
+# Create a function that alternates the case of the letters in a string.
+# The first letter should always be UPPERCASE.
+
+def alternating_caps(txt):
+    
+
+
+print(alternating_caps("Hello")) # "HeLlO"
+print(alternating_caps("Hey, how are you?")) # "HeY, hOw aRe yOu?"
+print(alternating_caps("OMG!!! This website is awesome!!")) # "OmG!!! tHiS WeBsItE Is aWeSoMe!!"
