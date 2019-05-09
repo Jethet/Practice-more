5dd53cb934cfad3ea093821017d2ed72beada5f9
Jethet <henriette.hettinga@gmail.com>
2019-05-09 10:52:37 +0200

    Edabit function to calculate area triangle

---
+++ EdabTriangleArea.py
@@ -0,0 +1,9 @@
+# Write a function that takes the base and height of a triangle and
+# returns its area.
+
+def tri_area(base, height):
+    return int((base * height) / 2)
+
+print(tri_area(3, 2)) # 3
+print(tri_area(7, 4)) # 14
+print(tri_area(10, 10)) # 50
