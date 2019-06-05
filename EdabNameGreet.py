6b0e51aec5390b63e66655b54ddcab6d00941d8b
Jethet <henriette.hettinga@gmail.com>
2019-06-05 18:25:18 +0200

    Add exclamation mark

--- EdabNameGreet.py
+++ EdabNameGreet.py
@@ -1,7 +1,7 @@
 # Create a function that takes a name and returns a greeting.
 
 def hello_name(name):
-    return "Hello {}".format(name)
+    return "Hello {}!".format(name)
 
 print(hello_name("Gerald")) #➞ "Hello Gerald!"
 print(hello_name("Tiffany")) #➞ "Hello Tiffany!"
