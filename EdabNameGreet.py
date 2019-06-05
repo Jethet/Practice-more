afc48ce072b6542b39b4851595493338b3476610
Jethet <henriette.hettinga@gmail.com>
2019-06-05 18:20:49 +0200

    Edabit greet function

---
+++ EdabNameGreet.py
@@ -0,0 +1,8 @@
+# Create a function that takes a name and returns a greeting.
+
+def hello_name(name):
+    return "Hello {}".format(name)
+
+print(hello_name("Gerald")) #➞ "Hello Gerald!"
+print(hello_name("Tiffany")) #➞ "Hello Tiffany!"
+print(hello_name("Ed")) #➞ "Hello Ed!"
