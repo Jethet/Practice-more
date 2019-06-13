7a95b69a09856cfecae77953757234f5de113f0d
Jethet <henriette.hettinga@gmail.com>
2019-06-13 10:59:34 +0200

    Edabit ASCII value of char

---
+++ EdabChar_ASCII.py
@@ -0,0 +1,9 @@
+# Create a function that returns the ASCII value of the passed in character.
+
+def ctoa(char):
+    return ord(char)
+
+print(ctoa("A")) #➞ 65
+print(ctoa("m")) #➞ 109
+print(ctoa("[")) #➞ 91
+print(ctoa("/")) #➞ 92
