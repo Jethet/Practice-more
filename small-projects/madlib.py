488006aecc4a2e2a5600f5ae65bdc38ef33d490b
Jethet <henriette.hettinga@protonmail.com>
2022-07-23 16:13:03 +0200

    Add basic madlib; notes on fstring

---
+++ small-projects/madlib.py
@@ -0,0 +1,6 @@
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+word3 = input("Enter the third word: ")
+
+result = f"This is not a {word1} text, you have to {word2} words by yourself to make it {word3}"
+print(result)
