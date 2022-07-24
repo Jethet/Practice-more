3459e31caba0daea2a0d4e7b09cf535510b1095b
Jethet <henriette.hettinga@protonmail.com>
2022-07-24 14:42:20 +0200

    Second version guessing game

--- small-projects/randomNum.py
+++ small-projects/randomNum.py
@@ -2,11 +2,19 @@ import random
 
 # generate random number
 # randomNum = (random.randrange(0,10)) 
-compNum = random.randrange(1, 20)
 
-myNum = int(input("Please enter a number between 1 and 20: "))
+def guessingGame(x):
+  compNum = random.randrange(1, x)
+  guess = 0
+  
+  while guess != compNum:
+    guess = int(input(f"Please enter a number between 1 and {x}: "))
+  
+    if guess < compNum:
+      print("Your guess is too low")
+    elif guess > compNum:
+      print("Your guess is too high")
+    elif guess == compNum:
+      print("You have won!")
 
-if myNum == compNum:
-  print("You have won!")
-else:
-  print("Try again")
+guessingGame(10)
