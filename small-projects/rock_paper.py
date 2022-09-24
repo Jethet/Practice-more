0f140c108763bfc5395134b97c546965494352c7
Jethet <henriette.hettinga@protonmail.com>
2022-09-24 16:31:51 +0200

    Final code

--- small-projects/rock_paper.py
+++ small-projects/rock_paper.py
@@ -2,13 +2,28 @@ import random
 
 # Game rules: rock beats scissors, scissors beat paper, paper beats rock
 
+
 def play():
-    user = input("'R' for Rock, 'P' for Paper, 'S' for scissors")
-    computer = random.choice(['R', 'P', 'S'])
+    user = input("What's your choice? 'R' for Rock, 'P' for Paper, 'S' for scissors\n")
+    computer = random.choice(["R", "P", "S"])
 
     if user == computer:
-        return 'Tie!'
+        return "It's a tie!"
 
-def is_winner(player, computer):
+    if is_winner(user, computer):
+        return "You won!"
+
+    return "You lost!"
+
+
+def is_winner(player, opponent):
     # returns 'true' if player wins
-    
+    if (
+        (player == "R" and opponent == "S")
+        or (player == "S" and opponent == "P")
+        or (player == "P" and opponent == "R")
+    ):
+        return True
+
+
+print(play())
