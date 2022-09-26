685d596f808fc88d81aa6381f7008bbd13db7553
Jethet <henriette.hettinga@protonmail.com>
2022-09-26 16:40:05 +0200

    Create player functions

--- small-projects/tictactoe/player.py
+++ small-projects/tictactoe/player.py
@@ -14,11 +14,28 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        # get a random valid spot for the next move
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0 to 9): ')
+            # check to see if it is a correct value: try to cast it into integer
+            # if not integer, it is invalid
+            # if spot not available on board, it is also invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square, try again!")
+                
+        return val
