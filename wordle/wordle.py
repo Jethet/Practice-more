449d63fb6bbb1afd09e95f3854f95c17398b41ca
Jethet <henriette.hettinga@protonmail.com>
2023-01-05 12:48:01 +0100

    Reformat

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,10 +1,10 @@
 import random
 from words import wordList
 
-
 blue = '\033[94m' 
 red = '\033[91m' 
 green = '\033[92m'
+reset = '\033[0m'
 
 def randomWord():
     randWord = random.choice(wordList)
@@ -14,13 +14,16 @@ def game(randWord):
     guess = input("Please enter your word: ")
     guessNumber = 0
 
-    for x in guess:
-        if x in randWord:
-            print(x)
+    while guessNumber < 5:
+        for x in guess:
+            if x in randWord:
+                print(f'{green}', x)
 
-    print(randWord)
+            if x not in randWord:
+                print(f'{red}', x)
+        print(randWord)
 
-    guessNumber += 1
+        guessNumber += 1
     # print(guessNumber)
             
 
