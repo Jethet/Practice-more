e544cf8d46f1ff65e9f9c78f13acbf191be793a3
Jethet <henriette.hettinga@protonmail.com>
2023-01-05 18:01:52 +0100

    Fix problem with loop and counter

--- wordle/test.py
+++ wordle/test.py
@@ -12,29 +12,26 @@ def randomWord():
 
 def game(randWord):
     guessNumber = 0
-
-    if guessNumber < 5:
+    
+    while guessNumber < 5:
         guess = input("Please enter your word: ")
 
         for x in guess:
-            while guessNumber < 5:
-                if x in randWord:
-                #     print(f'{blue}', x)
-                # if x not in randWord:
-                #     print(f'{red}', x)
-                    indexGuessChar = [i for i, b in enumerate(guess) if b == x]
-                    indexWordChar = [i for i, a in enumerate(randWord) if a == x]
-                    for b in indexGuessChar:
-                        for a in indexWordChar:
-                            if b == a:        
-                                print(f'{green}', x)
-                            else:
-                                print(f'{blue}', x)
-                else:
-                    print(f'{red}', x)
-            guessNumber += 1
+            if x in randWord:
+                indexGuessChar = [i for i, b in enumerate(guess) if b == x]
+                indexWordChar = [i for i, a in enumerate(randWord) if a == x]
+                for b in indexGuessChar:
+                    for a in indexWordChar:
+                        if b == a:        
+                            print(f'{green}', x)
+                        else:
+                            print(f'{blue}', x)
+            else:
+                print(f'{red}', x)
+                
+        guessNumber += 1
 
-            print(randWord)
-            print(guessNumber)
+        print(randWord)
+        print(guessNumber)
                 
 game(randomWord())
